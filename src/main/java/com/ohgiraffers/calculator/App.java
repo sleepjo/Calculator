1846b5644a727a404fabf0837ebf896e57b40876
choseungyoun <tmddbs9313@naver.com>
2024-03-06 15:51:05 +0900

    [승윤]

--- src/main/java/com/ohgiraffers/calculator/App.java
+++ src/main/java/com/ohgiraffers/calculator/App.java
@@ -20,9 +20,9 @@ public class App {
 
 
     public static int add(int x, int y){
-        return 0;
-    }
+        return x+y;
 
+    }
     public static int subtract(int x, int y){
         return 0;
     }
