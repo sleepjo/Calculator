f164f35719d542cc5dd59895302a72ce0502e211
kenryu <jryu1521@gmail.com>
2024-03-06 15:50:06 +0900

    main done

--- src/main/java/com/ohgiraffers/calculator/App.java
+++ src/main/java/com/ohgiraffers/calculator/App.java
@@ -6,12 +6,41 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class App {
+    static int n1, n2, ans;
+    static char op;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
         do{
+            System.out.println("quit: 0 + 0");
+            System.out.print("Input: ");
             st = new StringTokenizer(br.readLine());
+            n1 = Integer.parseInt(st.nextToken());
+            op = st.nextToken().charAt(0);
+            n2 = Integer.parseInt(st.nextToken());
+            if(n1 == n2 && n1 ==0) break;
+            switch(op){
+                case '+':
+                    ans = add(n1,n2);
+                    break;
+
+                case '-':
+                    ans = subtract(n1,n2);
+                    break;
+                case '*':
+                    ans = multiply(n1,n2);
+                    break;
+                case '/':
+                    ans = divide(n1,n2);
+                    break;
+                case '%':
+                    ans = modular(n1,n2);
+                    break;
+            }
+
+
+            System.out.println("Output: "+ ans);
 
 
         } while(true);
@@ -27,6 +56,9 @@ public class App {
         return 0;
     }
 
+    public static int multiply(int x, int y){
+        return 0;
+    }
     public static int divide(int x, int y){
         return 0;
     }
